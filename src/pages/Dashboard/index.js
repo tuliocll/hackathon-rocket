5f4be25e5354173bd92cd9db4db706f7dba44636
Gabriel Batista <svbgabriel@gmail.com>
2019-11-27 12:51:26 -0300

    Add Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,20 +7,92 @@ import {
   Separator,
   RightContainer,
   NameContainer,
-  NameText,
+  Wrapper,
+  CourseContainer,
+  CourseContent,
+  ContentWrapper,
+  Starter,
+  CourseContentStarter,
+  Courses,
 } from './styles';
+
+import avatar from '../../assets/images/avatar.png';
+import coin from '../../assets/images/coin.png';
+import monitor from '../../assets/images/monitor.png';
+import facebook from '../../assets/images/facebook.png';
 
 export default function Dashboard() {
   return (
     <Container>
       <LeftContainer>
-        <Letter>G</Letter>
+        <Letter>K</Letter>
         <Separator />
+        <Wrapper>
+          <img src={monitor} alt="monitor" />
+          <img src={coin} alt="coin" />
+          <div>
+            <img src={avatar} alt="avatar" />
+          </div>
+        </Wrapper>
       </LeftContainer>
       <RightContainer>
         <NameContainer>
-          <NameText>Bem vindo, Guilherme</NameText>
+          <p>Bem vindo, Kleiton</p>
         </NameContainer>
+        <Courses>
+          <CourseContainer>
+            <ContentWrapper>
+              <CourseContent>
+                <p>LP</p>
+              </CourseContent>
+              <CourseContent>
+                <p>LP</p>
+              </CourseContent>
+            </ContentWrapper>
+            <ContentWrapper>
+              <CourseContent>
+                <p>LP</p>
+              </CourseContent>
+              <CourseContent>
+                <p>LP</p>
+              </CourseContent>
+            </ContentWrapper>
+            <CourseContentStarter>
+              <Starter>LP</Starter>
+            </CourseContentStarter>
+          </CourseContainer>
+          <CourseContainer>
+            <ContentWrapper>
+              <CourseContent>
+                <a href="/game">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </CourseContent>
+              <CourseContent>
+                <a href="/game">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </CourseContent>
+            </ContentWrapper>
+            <ContentWrapper>
+              <CourseContent>
+                <a href="/game">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </CourseContent>
+              <CourseContent>
+                <a href="/game">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </CourseContent>
+            </ContentWrapper>
+            <CourseContentStarter>
+              <a href="/game">
+                <img src={facebook} alt="facebook" />
+              </a>
+            </CourseContentStarter>
+          </CourseContainer>
+        </Courses>
       </RightContainer>
     </Container>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   height: 100%;
+  width: 100%;
 `;
 
 export const LeftContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  position: absolute;
   width: 10%;
   height: 100%;
   max-width: 200px;
@@ -19,26 +18,135 @@ export const Letter = styled.p`
   font-size: 30px;
   color: #fff;
   margin: 10px 0;
+  text-align: center;
 `;
 
 export const Separator = styled.div`
   height: 1px;
   background-color: #fff;
   width: 90%;
+  margin: 0 auto;
 `;
 
 export const NameContainer = styled.div`
   display: flex;
-`;
+  width: 100%;
+  align-items: flex-end;
+  justify-content: flex-end;
+  margin-bottom: 20px;
 
-export const NameText = styled.p`
-  font-weight: bold;
-  font-size: 24px;
-  color: #fff;
+  > p {
+    font-weight: bold;
+    font-size: 24px;
+    color: #fff;
+  }
 `;
 
 export const RightContainer = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
   padding: 20px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  img:first-child {
+    margin: 48px 0;
+  }
+`;
+
+export const CourseContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  flex-direction: column;
+  width: 392px;
+  height: 462px;
+
+  background: #34353f;
+  border-radius: 7px;
+  margin-left: 15%;
+  margin-top: 20px;
+  padding-top: 96px;
+  position: relative;
+`;
+
+export const CourseContent = styled.div`
+  width: 120px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #ffffff;
+  /* border: 20px solid #262730;*/
+  border-radius: 7px;
+
+  p {
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 24px;
+    text-align: center;
+    color: #000000;
+    background-color: #ede407;
+    width: 60px;
+    height: 60px;
+    display: block;
+    line-height: 60px;
+    border-radius: 7px;
+  }
+`;
+
+export const Starter = styled.div`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 24px;
+  text-align: center;
+  color: #000000;
+  background-color: #ede407;
+  width: 60px;
+  height: 60px;
+  display: block;
+  line-height: 60px;
+  border-radius: 7px;
+`;
+
+export const CourseContentStarter = styled.div`
+  width: 120px;
+  height: 120px;
+  display: flex;
+  border: 20px solid #100f12;
+  border-radius: 7px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 15px;
+  right: 50%;
+  transform: translate(0, 0);
+
+  background: #ffffff;
+  /* border: 20px solid #262730;*/
+  border-radius: 7px;
+
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  top: -65px;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Courses = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
