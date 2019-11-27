961a2b0a1ad94d6db45f436fa8004c8e10358792
tulio <tuliocll@gmail.com>
2019-11-27 12:04:47 -0300

    :art: improbing the codebox component

--- src/App.js
+++ src/App.js
@@ -1,126 +1,19 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { IoIosFlash } from 'react-icons/io';
-import {
-  NotificationContainer,
-  NotificationManager,
-} from 'react-notifications';
-import { Circle } from 'react-awesome-spinners';
+import { NotificationContainer } from 'react-notifications';
 
 import Routes from './routes';
 
-import GlobalStyles, {
-  Container,
-  Button,
-  ContainerFooter,
-} from './styles/global.styles';
-
-import CodeBox from './components/Codebox';
-import Playground from './components/Playground';
-
-import nav1 from './assets/images/facebook/nav1.png';
-import nav2 from './assets/images/facebook/nav2.png';
-import nav3 from './assets/images/facebook/nav3.png';
-import nav4 from './assets/images/facebook/nav-4.png';
+import GlobalStyles from './styles/global.styles';
 
 import 'react-notifications/lib/notifications.css';
 
 export default function App() {
-  const [code, setCode] = useState();
-  const [compiling, setCompiling] = useState(false);
-
-  function compilar(str) {
-    setCompiling(true);
-    setTimeout(() => {
-      if (str === '<div class="header"></div>') {
-        return draw('nav1');
-      }
-
-      if (str === '<div class="header"><h1 class="logo">Facebook</h1></div>') {
-        return draw('nav2');
-      }
-
-      if (
-        str ===
-        '<div class="header"><h1 class="logo">Facebook</h1></div><div><nav class="nav-link"><i class="home"></i></nav></div>'
-      ) {
-        return draw('nav3');
-      }
-
-      if (
-        str ===
-        '<div class="header"><h1 class="logo">Facebook</h1></div><div><nav class="nav-link"><i class="home"></i><i class="groups"></i><i class="video"></i><i class="store"></i><i class="alert"></i><i class="menu"></i></nav></div>'
-      ) {
-        return draw('nav4');
-      }
-
-      NotificationManager.error(
-        'Verifique seu codigo e tente novamente!',
-        'Erro de compilação'
-      );
-      setCompiling(false);
-    }, Math.floor(Math.random() * 3000) + 1500);
-  }
-
-  function draw(element) {
-    setCompiling(false);
-    NotificationManager.success('Compilado com sucesso!');
-    const c = document.getElementById('playground');
-    const ctx = c.getContext('2d');
-
-    if (element === 'nav1') {
-      var img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, 0, 0, 300, 50);
-      };
-      img.src = nav1;
-
-      return;
-    }
-
-    if (element === 'nav2') {
-      var img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, 0, 0, 300, 50);
-      };
-      img.src = nav2;
-      return;
-    }
-
-    if (element === 'nav3') {
-      var img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, 0, 0, 300, 50);
-      };
-      img.src = nav4;
-      return;
-    }
-    if (element === 'nav4') {
-      var img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, 0, 0, 300, 50);
-      };
-      img.src = nav3;
-      return;
-    }
-  }
   return (
     <Router>
-      {/* <Routes /> */}
+      <Routes />
 
       <GlobalStyles />
-      <Container>
-        <CodeBox value={code} setValue={setCode} />
-        <Playground />
-      </Container>
-
-      <ContainerFooter>
-        <Button disabled={compiling} onClick={() => compilar(code)}>
-          Executar
-          <IoIosFlash />
-          {compiling && <Circle size={20} />}
-        </Button>
-      </ContainerFooter>
       <NotificationContainer />
     </Router>
   );
